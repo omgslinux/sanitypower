447792c4158f31722b6d27a13f1328e63077b302
omgslinux <omgs01@gmail.com>
2022-12-30 15:41:20 +0100

    CRUD de usuarios

---
+++ src/Form/UserType.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add(
+                'plainPassword',
+                RepeatedType::class,
+                [
+                    'type' => PasswordType::class,
+                    'required' => $options['new'],
+                    'first_options' =>
+                    [
+                        'label' => 'Password',
+                    ],
+                        'second_options' =>
+                    [
+                        'label' => 'Confirm password',
+                    ],
+                    'attr' => [
+                        'autocomplete' => 'off'
+                    ]
+                ]
+            )
+            ->add('admin')
+            ->add(
+                'startDate',
+                null,
+                [
+                    'widget' => 'single_text',
+                ]
+            )
+            ->add(
+                'endDate',
+                null,
+                [
+                    'widget' => 'single_text',
+                ]
+            )
+            ->add('active')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'new' => true,
+        ]);
+    }
+}
